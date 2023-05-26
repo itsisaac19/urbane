bcfecfbe1809a0243f1d6e14447b71dc565b309e
itsisaac19 <64609807+itsisaac19@users.noreply.github.com>
2023-05-26 00:22:10 -0500

    Preact Unsupported

--- next.config.js
+++ next.config.js
@@ -6,17 +6,6 @@ const nextConfig = {
     appDir: true,
   },
   reactStrictMode: false,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
 };
 
 if (process.env.ANALYZE === 'true') {
